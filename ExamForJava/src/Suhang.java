ecc34be3e8b6f586bb271963c8f615098fdd0b33
Linkshimcat <axzenone@gmail.com>
2026-05-26 14:49:01 +0900

    ReLearn

---
+++ ExamForJava/src/Suhang.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+public class Suhang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        #1번 문제
+//        System.out.print("정수 입력: ");
+//        int num = sc.nextInt();
+//
+//        if (num > 0) {
+//            System.out.println("plus");
+//        }
+//        else if (num < 0) {
+//            System.out.println("minus");
+//        }
+//
+//        if(num % 2 == 0) {
+//            System.out.println("odd");
+//        }
+//        else {
+//            System.out.println("s");
+//        }
+
+//        #2번 문제
+//        System.out.print("맞힌 개수: ");
+//        int num = sc.nextInt();
+//
+//        switch (num) {
+//            case 6 : System.out.println("1등"); break;
+//            case 5 : System.out.println("2등"); break;
+//            case 4, 3 : System.out.println("3등"); break;
+//            default :  System.out.println("낙첨");
+//        }
+
+
+//      #3번 문제
+//        System.out.print("개수: ");
+//        int num = sc.nextInt();
+//
+//        for (int i = 1; i<=num; i++) {
+//            if (i % 5 == 0) {
+//                System.out.print("뽀용");
+//            }
+//            else {
+//                System.out.print(i);
+//            }
+//        }
+//        System.out.println();
+
+//        #4번 Parking
+//        System.out.print("주차 시간(분): ");
+//        int time = sc.nextInt();
+//
+//        if (time <= 0) {
+//            System.out.println("잘못된 입력");
+//        } else {
+//            int fare = (time / 10) * 300;
+//            if (time > 100) fare -= 2000;
+//            System.out.println("주차 요금: " + (((fare / 10) > 0) ? (fare/10) +  "," : "") +
+//                    (((fare % 1000) == 0) ? "000" : (fare % 1000)));
+//        }
+
+//        #5번
+//        System.out.print("학생 수 : ");
+//        int N = sc.nextInt();
+//        int sum = 0;
+//        int cnt = 0;
+//
+//        for(int i=1; i<=N; i++) {
+//            System.out.print(i + "번 학생 키 : ");
+//            int height = sc.nextInt();
+//            System.out.print(i + "번 학생 " + height + "cm - ");
+//            if(height >= 140 && height <= 190) {
+//                System.out.println("탑승 가능");
+//                sum += height;
+//                cnt++;
+//            }
+//            else {
+//                System.out.println("탑승 불가");
+//            }
+//        }
+//        System.out.println("-------------------");
+//        System.out.print("탑승 가능한 학생의 평균키 : ");
+//        if(cnt > 0) {
+//            System.out.print(((float)sum / cnt));
+//        }
+//        else {
+//            System.out.print("-1");
+//        }
+//
+//        sc.close();
+//    }
+
+
+    }
+}
