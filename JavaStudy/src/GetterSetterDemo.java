7a4e02126c950679361ee01b441c972ef8c97965
Linkshimcat <axzenone@gmail.com>
2026-05-28 15:43:55 +0900

    Class Getter & Setter Solve

---
+++ JavaStudy/src/GetterSetterDemo.java
@@ -0,0 +1,47 @@
+class CarwithGetterSetter {
+//    필드 3개
+    String brand;
+    String model;
+    int year;
+
+//    게터 메서드
+//    get + 필드이름(맨 앞글자를 대문자)
+//    예시: getYear , getBrand
+//    게터 메서드는 매개변수가 없음
+
+    //  brand
+    String getBrand() {
+        return brand;
+    }
+    //  model
+    String getModel() {
+        return model;
+    }
+    //    year
+    int getYear() {
+        return year;
+    }
+
+    //    세터 메서드는 그 필드값을 대입해서 값을 부여하는 메서드
+    void setBrand(String b) {
+        brand = b;
+    }
+    //    세터 setModel
+    void setModel(String c) {
+        model = c;
+    }
+    //    세터 setModel
+    void setYear(int d) {
+        year = d;
+    }
+
+}
+
+public class GetterSetterDemo {
+    public static void main(String[] args) {
+        CarwithGetterSetter o2 = new CarwithGetterSetter();
+        o2.setBrand("현대 자동차");
+        System.out.println((o2.getBrand()));
+
+    }
+}
---
+++ JavaStudy/src/SolveBankAccount.java
@@ -0,0 +1,26 @@
+class MyBankAccount {
+    int balance;
+
+    int getBalance() {
+        return balance;
+    }
+
+    void setBalance(int money) {
+        if(money < 0) {
+            System.out.println("잘못된 금액입니다.");
+            return;
+        }
+        balance = money;
+    }
+
+}
+
+public class SolveBankAccount {
+    public static void main(String[] args) {
+        MyBankAccount bank = new MyBankAccount();
+        bank.setBalance(10000);
+        System.out.println("잔액: " + (bank.getBalance()));
+        bank.setBalance(-1000);
+        System.out.println("잔액: " + (bank.getBalance()));
+    }
+}
