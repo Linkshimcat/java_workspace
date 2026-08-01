70e72680bbaae31b07d5f11a16f7da4b2a80acf3
Linkshimcat <axzenone@gmail.com>
2026-08-02 00:57:41 +0900

    Summer 과제

--- JavaStudy/src/proj1211/Memeber_1211.java
+++ JavaStudy/src/proj1211/Memeber_1211.java
@@ -1,4 +1,49 @@
+// 1211 이윤재
+
 package proj1211;
+import java.util.Scanner;
+
+class Member {
+    String name;
+    int age;
+    int point;
+
+    Member(String name, int age, int point) {
+        this.name = name;
+        this.age = age;
+        this.point = point;
+    }
+
+    void setAge(int age) {
+        if (age < 0) {
+            System.out.println("유효하지 않은 나이입니다.");
+        } else {
+            this.age = age;
+        }
+    }
+
+    String getInfo() {
+        return "이름: " + name + " | 나이: " + age + "세 | 포인트: " + point;
+    }
+}
 
 public class Memeber_1211 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("이름 :");
+        String namePeople = sc.nextLine();
+        System.out.print("나이 :");
+        int agePeople = sc.nextInt();
+        System.out.print("포인트 :");
+        int pointPeople = sc.nextInt();
+
+        Member people = new Member(namePeople, agePeople, pointPeople);
+        System.out.println(people.getInfo());
+
+        System.out.print("변경할 나이: ");
+        int cdAge = sc.nextInt();
+
+        people.setAge(cdAge);
+        System.out.println(people.getInfo());
+    }
 }
--- JavaStudy/src/proj1211/MovieTest_1211.java
+++ JavaStudy/src/proj1211/MovieTest_1211.java
@@ -1,4 +1,54 @@
+//1211 이윤재
 package proj1211;
+import java.util.Scanner;
+
+class Movie {
+    String movieTitle;
+    String director;
+    int movieTime;
+
+    // 매게변수가 있는 생성자로 (괄호 안) 입력
+    Movie(String movieTitle, String director, int movieTime) {
+        this.movieTitle = movieTitle;
+        this.director = director;
+        this.movieTime = movieTime;
+    }
+
+    String getMovieInfo() {
+        return movieTitle + "\t     " + director + "\t      " + movieTime;
+    }
+
+}
 
 public class MovieTest_1211 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 중복되는 개체가 많으니 클래스 배열 사용
+        Movie[] movies = new Movie[5];
+
+        // 반복문으로
+        for (int i = 0; i < movies.length; i++) {
+            System.out.print("영화" + (i + 1) + " 제목: ");
+            String inputTitle = sc.next();
+            System.out.print("영화" + (i + 1) + " 감독명: ");
+            String inputDirector = sc.next();
+            System.out.print("영화" + (i + 1) + " 상영시간(분): ");
+            int inputTime = sc.nextInt();
+            // 인스턴스 객체 새로 생성
+            movies[i] = new Movie(inputTitle, inputDirector, inputTime);
+        }
+
+        System.out.println("------------------------");
+        System.out.println("영화제목 \t 감독명  \t  상영시간");
+
+        int totalTime = 0;
+        for (int i = 0; i < movies.length; i++) {
+            totalTime += movies[i].movieTime;
+            System.out.println(movies[i].getMovieInfo());
+        }
+
+        System.out.println("------------------------");
+        System.out.println("총 상영시간: " + totalTime + "분");
+
+    }
 }
