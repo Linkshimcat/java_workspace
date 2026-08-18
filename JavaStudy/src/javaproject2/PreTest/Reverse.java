f6c396cfaefd427170dd2524354f2bfd111ae810
Linkshimcat <axzenone@gmail.com>
2026-08-18 21:00:33 +0900

    2학기 자바 프로젝트

---
+++ JavaStudy/src/javaproject2/PreTest/Reverse.java
@@ -0,0 +1,25 @@
+package javaproject2.PreTest;
+
+import java.util.Scanner;
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int cnt = 0;
+        while(temp > 0) {
+//            반복문으로 temp를 10으로 나누기
+            temp = temp / 10;
+            cnt++;
+        }
+        int[] data = new int[cnt];
+        for (int i = 0; i < cnt; i++) {
+            data[i] = n % 10;
+            n = n / 10;
+        }
+        for (int i = 0; i < cnt; i++) {
+            System.out.print(data[i]);
+        }
+
+    }
+}
---
+++ JavaStudy/src/javaproject2/nArray.java
@@ -0,0 +1,30 @@
+package javaproject2;
+
+import java.util.Scanner;
+public class nArray {
+    // 내림차순 배열
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String ncd = n + "";
+        for (int i = 0; i < ncd.length(); i++) {
+            char s = ncd.charAt(i);
+            System.out.println(s);
+        }
+
+        int[] array = new int[5];
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+
+        }
+        for (int j = 0; j < array.length; j++) {
+            int temp = 0;
+
+        }
+        System.out.println(result);
+    }
+
+}
+
+
+
